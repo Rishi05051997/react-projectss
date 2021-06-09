3cbd77aab16f10294ebd3b84e7ab8fc34649b7ce
rishi05051997 <vrushabhdhatral10@gmail.com>
2021-06-09 18:06:21 +0530

    all static website

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,10 +1,48 @@
 import './App.css';
 import React from "react";
 
-function App() {
+import Navbar from './components/Navabar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Service from './components/service';
+import Portfolio from './components/portfolio';
+
+import Pricing from './components/Pricing';
+import Contact from './components/Contact';
+import Image from './components/Image';
+//// component function name 1st letter is always capital
+// function Welcome() {
+//   return <h5>Welcome back Vrushabh</h5>
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       < Navbar />
+//       < Hero />
+//       < About />
+//       {/* < Welcome/> */}
+
+
+//     </div>
+//   );
+// }
+
+// flat arrow function 
+const App = () => {
   return (
     <div className="App">
-      <h1>My First React App</h1>
+      < Navbar />
+      < Hero />
+      < About />
+      < Service />
+      < Portfolio />
+      < Pricing />
+      < Contact />
+      < Image />
+      {/* < Welcome/> */}
+
+
     </div>
   );
 }
---
+++ my-first-app/src/components/Image.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React, { Fragment } from 'react';
+
+const Image = () => {
+    return (
+        <Fragment>
+            {/* <!-- Image of location/map --> */}
+          <footer className="container-fluid text-center">
+            <a href="#myPage" title="To Top">
+              <span className="glyphicon glyphicon-chevron-up"></span>
+            </a>
+            <p>Bootstrap Theme Made By <a href="http://vrushabhdhatak10-portfolio.netlify.app/" title="Visit w3schools">Vrushabh Dhatrak</a></p>
+          </footer>
+        </Fragment>
+    )
+}
+
+export default Image;
